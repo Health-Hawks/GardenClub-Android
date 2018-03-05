0b84b735c763b5a792324456e23c3f05f7149c58
jbr5433 <31827506+jbr5433@users.noreply.github.com>
2018-03-05 10:13:51 -0500

    app is connected to the MySQL database
    can only connect if the user can access satoshi.cis.uncw.edu

--- GardenClub/app/src/main/java/com/example/lenovo/gardenclub/MainActivity.java
+++ GardenClub/app/src/main/java/com/example/lenovo/gardenclub/MainActivity.java
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.BufferedReader;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    EditText UsernameEt, PasswordEt;
     Button Login;
 
     @Override
@@ -20,19 +20,28 @@ public class MainActivity extends AppCompatActivity {
 
         Login = (Button) findViewById(R.id.button);
 
-
-
+        UsernameEt = (EditText)findViewById(R.id.et_loginID);
+        PasswordEt = (EditText)findViewById(R.id.et_pass);
 
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String username = UsernameEt.getText().toString();
+                String password = PasswordEt.getText().toString();
+                String type = "login";
+                BackgroundWorker backgroundWorker = new BackgroundWorker(MainActivity.this);
+                backgroundWorker.execute(type, username, password);
 
-
-                Intent intent = new Intent(MainActivity.this, ContactList.class);
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, ContactList.class);
+//                startActivity(intent);
      //           Login.setText("test");
 
             }
         });
     }
+
+    public void OnLogin(View view) {
+
+    }
+
 }
